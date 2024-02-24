2fd1e9ddd5151400cd62015baa334c3a63740944
cookeda <cookeda@appstate.edu>
2024-02-24 12:57:17 -0500

    n/a

--- CDL/CDL/spiders/teams.py
+++ CDL/CDL/spiders/teams.py
@@ -15,11 +15,6 @@ class TeamsSpider(scrapy.Spider):
             yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
-#        for x in range(1, len(response.css('li.product__item'))+1):
-        
-        name = response.css('li.product__item:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > h2:nth-child(1) > a:nth-child(1)::text').get()
-            #link = response.css(f'div:nth-child({x}) > div:nth-child(1) > h2:nth-child(1) > a:nth-child(1)::attr(href)').get()
-        yield {
-                'Name': name,
-            #    'Link': link    
-            }
+        for team in response.css('li.product__item'):
+            name = team.css('h2 > a::text').get()  # Extract the team name
+            yield {'name': name} 
